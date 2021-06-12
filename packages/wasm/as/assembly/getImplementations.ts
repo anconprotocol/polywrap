7956566c0e3076ce17918b2a4ba155f2d9e63952
dOrgJelli <jelli@dorg.tech>
2021-06-12 14:26:10 -0500

    simpler interface

--- packages/wasm/as/assembly/getImplementations.ts
+++ packages/wasm/as/assembly/getImplementations.ts
@@ -1,23 +1,25 @@
-// Get Implementations API
+import {
+  Read,
+  ReadDecoder
+} from "./msgpack";
 
+// Get Implementations API
 @external("w3", "__w3_get_implementations")
 export declare function __w3_get_implementations(
   uri_ptr: u32, uri_len: u32
 ): bool;
 
 // Get Implementations Result
-@external("w3", "__w3_get_implementations_result_array_len")
-export declare function __w3_get_implementations_result_array_len(): u32;
-@external("w3", "__w3_get_implementations_result_item_len")
-export declare function __w3_get_implementations_result_item_len(index: u32): u32;
-@external("w3", "__w3_get_implementations_result_item")
-export declare function __w3_get_implementations_result_item(index: u32, ptr: u32): void;
+@external("w3", "__w3_get_implementations_result_len")
+export declare function __w3_get_implementations_result_len(): u32;
+@external("w3", "__w3_get_implementations_result")
+export declare function __w3_get_implementations_result(ptr: u32): void;
 
 // Get Implementations Error
-// @external("w3", "__w3_get_implementations_error_len")
-// export declare function __w3_get_implementations_error_len(): u32;
-// @external("w3", "__w3_get_implementations_error")
-// export declare function __w3_get_implementations_error(ptr: u32): void;
+@external("w3", "__w3_get_implementations_error_len")
+export declare function __w3_get_implementations_error_len(): u32;
+@external("w3", "__w3_get_implementations_error")
+export declare function __w3_get_implementations_error(ptr: u32): void;
 
 export function w3_getImplementations(uri: string): string[] {
   const uriBuf = String.UTF8.encode(uri);
@@ -26,24 +28,26 @@ export function w3_getImplementations(uri: string): string[] {
     uriBuf.byteLength
   );
 
-  // if (!success) {
-  //   const errorLen = __w3_get_implementations_error_len();
-  //   const messageBuf = new ArrayBuffer(changetype<u32>(errorLen));
-  //   __w3_get_implementations_error(changetype<u32>(messageBuf));
-  //   const message = String.UTF8.decode(messageBuf);
-  //   throw new Error(message);
-  // }
+  if (!success) {
+    const errorLen = __w3_get_implementations_error_len();
+    const messageBuf = new ArrayBuffer(changetype<u32>(errorLen));
+    __w3_get_implementations_error(changetype<u32>(messageBuf));
+    const message = String.UTF8.decode(messageBuf);
+    throw new Error(message);
+  }
 
-  const arrayLen = __w3_get_implementations_result_array_len();
+  const resultLen = __w3_get_implementations_result_len();
+  const resultBuf = new ArrayBuffer(resultLen);
+  __w3_get_implementations_result(changetype<u32>(resultBuf));
+
+  // Decode the msgpack buffer
+  const decoder = new ReadDecoder(resultBuf);
   const result: string[] = [];
 
-  for (let i: u32 = 0; i < arrayLen; ++i) {
-    const strLen = __w3_get_implementations_result_item_len(i);
-    const strBuf = new ArrayBuffer(changetype<u32>(strLen));
-    __w3_get_implementations_result_item(i, changetype<u32>(strBuf));
-    const str = String.UTF8.decode(strBuf);
-    result.push(str);
-  }
+  decoder.readArray((reader: Read) => {
+    const item = reader.readString()
+    result.push(item);
+  });
 
   return result;
 }
